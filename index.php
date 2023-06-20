616a739b580a3c32c8405797944f449f4b2e6c60
Gustavo Lucas <101267569+gustavolucass@users.noreply.github.com>
2023-06-20 09:46:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/formulario.css" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="formulario.css" media="screen"/>
     <link
       rel="stylesheet"
       href="https://use.fontawesome.com/releases/v5.13.1/css/all.css"
